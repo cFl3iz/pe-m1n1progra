6aaedb34c9489ffca1ffcd56253469c3567dd1fa
s784838898 <784838898@qq.com>
2018-03-22 15:32:34 +0800

    卖家方向的一系列功能

--- pe-weminiapp/pages/editProduct/editProduct.js
+++ pe-weminiapp/pages/editProduct/editProduct.js
@@ -3,18 +3,106 @@ import Request from '../../utils/request.js'
 var app = getApp();
 Page({
   data: {
+    arrimg: [],           // 上传img的attr     => 页面显示的img                  
+    len: 4,              // 上传的img的最大的length
+    index: 0,         // 上传完成的个数
+    successArr: [],      // 存储上传返回的img的url =>发送的数据
+    questions: {},        // 提交数据存储到本地的josn
+    bool: true,  // 是否通过上传的权限
+    mun: 1,
+    tempFilePath: "",
+    picturePaths: '',
+    upLoadCount: 0,
+    productId: '',
     nowPartyId: '',
     productDetail: null,
     shareName: '',
     comments: [],
     availableToPromiseTotal: null,
-    contactTel: null
+    contactTel: null,
+    latitude: '',
+    longitude: '',
+    address: ''
   },
   onLoad: function (options) {
     var that = this
     console.log('this. data . productid = ' + options.productid)
+    this.setData(
+      {
+        productId: options.productid
+      }
+    )
     that.flushData(options.productid)
   },
+  chooseimage: function (e) {
+    this.chooseImageFn();   // 上传的fn
+  },
+
+  chooseImageFn: function () {   // 上传的fn 
+    var _this = this;
+    var len = _this.data.len;   // 获取data的上传的总个数
+    var mun = _this.data.index;  // 获取data的上传完成的个数
+    var arr = _this.data.arrimg;         // 获取data的img的list 
+    var suArr = _this.data.successArr; // 存储上传返回的img的src
+
+    // 调取手机的上传
+    wx.chooseImage({
+      count: 4,
+      sizeType: ['compressed'],
+      sourceType: ['album', 'camera'],
+      success: function (res) {       // 成功
+        console.log(res)
+        // var tempFilePaths = res.tempFilePaths[0].toString();
+        var filePathArray = res.tempFilePaths;
+        console.log('tempFilePaths=' + JSON.stringify(filePathArray))
+        let mun = _this.data.mun;
+        let dataIndex = _this.data.index;
+        console.log('->data Index = ' + dataIndex);
+        if (dataIndex != 0) {
+
+          let arrIndex = filePathArray.length;
+          console.log('data index in arrIndex=' + arrIndex);
+          for (let path of filePathArray) {
+            // len == mun ? mun = 4 : mun++;
+            if (_this.data.index <= 3) {// 上传之前的验证个数
+              arr.push(path);
+              _this.setData({
+                arrimg: arr
+              })
+              console.log('arrimg =' + _this.data.arrimg)
+            }
+          }
+          if (_this.data.index <= 3) {
+            _this.setData({
+              index: _this.data.index + arrIndex
+            })
+          }
+        } else {
+          for (let path of filePathArray) {
+            // len == mun ? mun = 4 : mun++;
+            if (_this.data.index <= 3) {// 上传之前的验证个数
+              arr.push(path);
+              _this.setData({
+                arrimg: arr,
+                index: mun
+              })
+              console.log('arrimg =' + _this.data.arrimg)
+            }
+            mun++;
+          }
+        }
+      }
+    })
+  },
+  previewImage: function (e) {
+    var current = e.target.dataset.src;
+    console.log('this.data.arrimg=' + this.data.arrimg) 
+    wx.previewImage({
+      current: current, // 当前显示图片的http链接  
+      urls: this.data.arrimg // 需要预览的图片http链接列表  
+    })
+  },
+
   flushData: function (productid) {
     console.log(' flush data >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
     var that = this
@@ -26,24 +114,47 @@ Page({
       (
       function (data) {
         console.log('return data = ' + JSON.stringify(data))
-        // console.log('data.resourceDetail=' + data)
-        if (data.resourceDetail == undefined) {
-          // that.setDemoData()
-        } else {
-          console.log('return data = ' + JSON.stringify(data))
-          that.setData({
-            nowPartyId: data.nowPartyId,
-            productDetail: data.resourceDetail,
-            shareName: data.resourceDetail.title,
-            comments: data.resourceDetail.tuCaoList,
-            availableToPromiseTotal: data.resourceDetail.availableToPromiseTotal,
-            contactTel: data.resourceDetail.contactNumber
-          })
-          wx.hideLoading()
+        var cover_url = data.resourceDetail.cover_url
+        var map = {
+          drObjectInfo: cover_url
+        };
+        data.resourceDetail.morePicture.unshift(map)
+        var tempArray = []
+        for (var key of data.resourceDetail.morePicture) {
+          　　 
+      　　　　console.log('key='+key);
+             tempArray.unshift("https://"+key.drObjectInfo)
         }
+
+        console.log('return data = ' + JSON.stringify(data))
+        that.setData({
+          nowPartyId: data.nowPartyId,
+          productDetail: data.resourceDetail,
+          shareName: data.resourceDetail.title,
+          comments: data.resourceDetail.tuCaoList,
+          availableToPromiseTotal: data.resourceDetail.availableToPromiseTotal,
+          contactTel: data.resourceDetail.contactNumber ,
+          arrimg: tempArray
+        })
+        wx.hideLoading()
+
 
       }
       )
+  },
+  closeImgFn: function (e) {
+    var doId = e.currentTarget.id;      // 对应的img的唯一id
+    console.log('del doId=' + doId)
+    var doarrimg = this.data.arrimg;    // 页面显示的img the list    
+    var doindex = this.data.index;   // 上传显示的个数
+    console.log('del doindex=' + doindex)
+    console.log('del doarrimg=' + doarrimg[doId])
+    doarrimg.splice(doId, 1);     // 删除当前的下标的数组
+    doindex--;       // 删除一个上传的个数就递减
+    this.setData({
+      arrimg: doarrimg,
+      index: doindex
+    })
   },
   formSubmit: function (e) {
     let that = this
@@ -61,15 +172,29 @@ Page({
       icon: 'success',
       duration: 20000
     })
-     
+
 
     that.releaseProduct(e)
+  },
+  getUserLocation: function () {
+    let that = this
+    wx.chooseLocation({
+      success: function (res) {
+        console.log('getUserLocation - > ' + res.latitude);
+        console.log('getUserLocation - > ' + res.longitude);
+        that.setData({
+          address: res.address,
+          latitude: res.latitude,
+          longitude: res.longitude
+        })
+      },
+    })
   },
   releaseProduct(e) {
     const that = this
 
     wx.showToast({
-      title: '正在保存..',
+      title: '正在更新..',
       icon: 'loading',
       duration: 10000
     })
@@ -81,6 +206,7 @@ Page({
     console.log('go to update resource' + that.data.latitude)
     var tel = that.data.contactTel
     const reqdata = {
+      productId: that.data.productId,
       title: e.detail.value.title,
       kuCun: e.detail.value.count,
       desc: e.detail.value.desc,
@@ -89,23 +215,17 @@ Page({
       address: e.detail.value.address,
       latitude: that.data.latitude,
       longitude: that.data.longitude,
-      tel: tel == null ? '' : tel
+      tel: e.detail.value.tel
     }
     Request.postRequest(url, reqdata).then(function (data) {
 
       console.log('update over - > = ' + JSON.stringify(data))
- 
-
-      wx.showModal({
-        title: '提示',
-        content: '更新完成',
-        showCancel: false,
-        success: function (res) {
-          if (res.confirm) {
-
-          }
-        }
+      wx.showToast({
+        title: '更新完毕',
+        icon: 'success',
+        duration: 2000
       })
+      that.flushData(that.data.productId)
     })
   },
 
--- pe-weminiapp/pages/myProduct/myProduct.js
+++ pe-weminiapp/pages/myProduct/myProduct.js
@@ -27,13 +27,54 @@ Page({
   touchEnd: function (e) {
     this.data.touchEndTime = e.timeStamp
   },
+  removeProductFromCategory:function(productId){
+    var that = this 
+    console.log('removeProductFromCategory =>' + productId)
+    const data = {
+      productId: productId
+    }
+    Request.postRequest('https://www.yo-pe.com/api/common/doRemoveProductFromCategory', data).then(function (data) {
+      wx.showToast({
+        title: '删除完毕',
+        icon: 'success',
+        duration: 2000
+      })
+      that.runderList()
+    })
+  },
   //长按
   longTap: function (e) {
+    var that = this
     console.log("long tap")
     wx.showModal({
       title: '提示',
       content: '永久删除?',
-      showCancel: true
+      showCancel: true,
+      success: function (res) {
+        if (res.confirm) {
+          var productid = e.currentTarget.dataset.productid
+          that.removeProductFromCategory(productid);
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
+      }
+    })
+    //doRemoveProductFromCategory
+  },
+  salesDiscontinuation:function(e){
+    var that  = this
+    console.log('salesDiscontinuation=>')
+    var productid = e.currentTarget.dataset.productid
+    const data = {
+      productId: productid
+    }
+    Request.postRequest('https://www.yo-pe.com/api/common/salesDiscontinuation', data).then(function (data) {
+      wx.showToast({
+        title: '下架完毕',
+        icon: 'success',
+        duration: 2000
+      })
+      that.runderList()
     })
   },
   runderList: function () {
